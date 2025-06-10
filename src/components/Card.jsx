317f91a2f34dcf3c353d69f11bd4835d7bbcb343
Eudald2000 <eudald08210@gmail.com>
2025-06-10 13:56:33 +0200

    last try

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,12 @@
-// Card.jsx
 export const Card = ({ image, volteada, onClick }) => {
+  const baseUrl = import.meta.env.BASE_URL.endsWith('/')
+    ? import.meta.env.BASE_URL
+    : import.meta.env.BASE_URL + '/'
+
   return (
     <div className="card" onClick={volteada ? null : onClick}>
       <img
-        src={volteada ? image : 'interrogante.jpg'}
+        src={volteada ? `${baseUrl}${image}` : `${baseUrl}interrogante.jpg`}
         alt="Carta juego"
         style={{ cursor: volteada ? 'default' : 'pointer' }}
       />
